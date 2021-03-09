dada7926aee1d04542a4eef6c22843efdb624980
David Downie <daviddownie@gmail.com>
2021-03-09 16:47:58 -0500

    refactored reader to use interface

--- typescriptCompleteDeveloperGuide/stats/src/index.ts
+++ typescriptCompleteDeveloperGuide/stats/src/index.ts
@@ -1,12 +1,15 @@
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
+import { CsvFileReader } from "./CsvFileReader";
 
-const reader = new MatchReader("football.csv");
-reader.read();
+const csvFileReader = new CsvFileReader("football.csv");
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let LiverpoolWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Liverpool" && match[5] === MatchResult.HomeWin) {
     LiverpoolWins++;
   } else if (match[2] === "Liverpool" && match[5] === MatchResult.AwayWin) {
